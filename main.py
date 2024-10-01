e387f62b23cfecf3073e81b381b6628d176f3b80
fabianwolski <fabianwolski03@gmail.com>
2024-10-01 11:23:39 +0100

    player object added to main - updates at each frame

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import pygame
 from constants import *
-# from module import function_hello, variable_player
+from player import *
+# from module import function
 
 def main():
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill((0,0,0))   
+        screen.fill((0,0,0)) 
+        player.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000.0
+        
+        player.update(dt)
+                
 if __name__ == "__main__":
     main()
