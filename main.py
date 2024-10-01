1e8e759d34a771f08a239026bfefc755a81197bf
fabianwolski <fabianwolski03@gmail.com>
2024-10-01 11:58:29 +0100

    grouping objects

--- main.py
+++ main.py
@@ -4,30 +4,39 @@ from player import *
 # from module import function
 
 def main():
+    #initialzing  game and screen
     pygame.init()
-
     clock = pygame.time.Clock()
-    dt = 0
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    #rendering player
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
-    
+    #groups
+    updatables = pygame.sprite.Group(player)
+    drawables = pygame.sprite.Group(player)
+    #container
+    Player.containers = (updatables, drawables)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill((0,0,0)) 
-        player.draw(screen)
+        
+        for drawable in drawables:
+            drawable.draw(screen)
 
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000.0
         
-        player.update(dt)
-                
+        for updateable in updatables:
+            updateable.update(dt)
+
 if __name__ == "__main__":
     main()
