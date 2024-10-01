0fce23425699492663cd506e1ac7cdd265a479e0
fabianwolski <fabianwolski03@gmail.com>
2024-10-01 11:20:23 +0100

    Added Player Class - Triangle object with Circle hitbox. Added move and rotation

---
+++ player.py
@@ -0,0 +1,49 @@
+import pygame
+from circleshape import *
+from constants import *
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+    #player will look like triangle
+    #hitbox logic - circle will be used
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen, (255,255,255), self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        #w,a,s,d and arrow keys
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            invert_dt = dt * -1
+            self.rotate(invert_dt)
+
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.rotate(dt)
+            
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.move(dt)
+
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.move(dt)
+
+        #arrows
+        if keys[pygame.K_LEFT]:
+            invert_dt = dt * -1
+            self.rotate(invert_dt)
+            
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt 
