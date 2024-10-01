22baad448ca8d9a374d591f6991e0280d56695ab
fabianwolski <fabianwolski03@gmail.com>
2024-10-01 12:50:52 +0100

    added asteroids + fixed group/container/instances logic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroids import Asteroid
+from asteroidfield import *
 # from module import function
 
 def main():
@@ -9,17 +11,22 @@ def main():
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    #rendering player
+    #groups
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    #container
+    Player.containers = (updatables, drawables)
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers = updatables
+    dt = 0
+
+    #instances
+    #rendering player and asteroids
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
-    #groups
-    updatables = pygame.sprite.Group(player)
-    drawables = pygame.sprite.Group(player)
-    #container
-    Player.containers = (updatables, drawables)
-
-    dt = 0
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
